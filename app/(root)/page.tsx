09c64f19a1e7766c4515a711cbf4faa713519d76
Yayifetty <yayinasir12@gmail.com>
2024-10-12 12:08:19 +0100

    css styling

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Section } from 'lucide-react'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <Section>
+        <div>
+            Home
+        </div>
+
+    </Section>
+  )
+}
+
+export default HomePage
